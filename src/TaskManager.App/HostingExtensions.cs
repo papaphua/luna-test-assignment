18d80a74486be86ed9c1edf1fd3f402ca31caa11
Yevhenii Nabok <1337niceshot@gmail.com>
2024-09-03 23:42:12 +0300

    add logging

---
+++ src/TaskManager.App/HostingExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace App;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.Run();
+
+        return app;
+    }
+}
