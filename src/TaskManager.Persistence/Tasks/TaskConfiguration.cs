bc60dbca777d4192a3bc2132e5d934712345a4e2
Yevhenii Nabok <1337niceshot@gmail.com>
2024-09-04 00:35:34 +0300

    add configuration

---
+++ src/TaskManager.Persistence/Tasks/TaskConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TaskManager.Persistence.Tasks;
+
+public sealed class TaskConfiguration : IEntityTypeConfiguration<Domain.Tasks.Task>
+{
+    public void Configure(EntityTypeBuilder<Domain.Tasks.Task> builder)
+    {
+        builder.HasKey(task => task.Id);
+        
+        builder.Property(task => task.Title)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(task => task.Description)
+            .HasMaxLength(1000);
+        
+        builder.Property(task => task.CreatedAt)
+            .IsRequired()
+            .HasDefaultValueSql("GETDATE()");
+        
+        builder.Property(task => task.UpdatedAt)
+            .IsRequired()
+            .HasDefaultValueSql("GETDATE()");
+    }
+}
