b232f10160c8bd737d7212fd201b6951a8551d32
Yevhenii Nabok <1337niceshot@gmail.com>
2024-09-04 00:15:39 +0300

    add unit of work

--- src/TaskManager.Persistence/ApplicationDbContext.cs
+++ src/TaskManager.Persistence/ApplicationDbContext.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using TaskManager.Application.Core;
 
 namespace TaskManager.Persistence;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : DbContext(options)
+    : DbContext(options), IUnitOfWork
 {
+    public async Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        return await Database.BeginTransactionAsync(cancellationToken);
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
